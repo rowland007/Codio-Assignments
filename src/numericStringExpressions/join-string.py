bab45a16f79d868f498485c8bf3760d59f6b2e28
Randy Rowland <rowland007@gmail.com>
2018-02-09 20:15:07 +0900

    Add Numeric and String Expression source

---
+++ src/numericStringExpressions/join-string.py
@@ -0,0 +1,7 @@
+
+# Input from the command line
+import sys
+name = sys.argv[1]
+
+# Write your code below
+print(name + ', hello!')
---
+++ src/numericStringExpressions/strnum.py
@@ -0,0 +1,9 @@
+
+# Input from the command line
+# converted to an integer
+import sys
+number1 = int(sys.argv[1])
+number2 = int(sys.argv[2])
+
+# Your code goes here
+print(str(number1) + str(number2))
